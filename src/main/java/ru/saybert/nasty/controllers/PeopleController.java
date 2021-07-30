4d1bf96b015dbe25b3b97dccc338037c45e8730b
nasty <nasty@saybert.ru>
2021-07-30 12:43:28 +0300

    Урок Алишева по DAO готово

--- src/main/java/ru/saybert/nasty/controllers/PeopleController.java
+++ src/main/java/ru/saybert/nasty/controllers/PeopleController.java
@@ -1,28 +1,33 @@
 package ru.saybert.nasty.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.management.modelmbean.ModelMBeanOperationInfo;
+import ru.saybert.nasty.dao.PersonDAO;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
+    private final PersonDAO personDAO;
+
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
     @GetMapping()
     public String index(Model model){
-        //Получим всех людей из DAO и передадим их на представление
-        return null;
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show (@PathVariable("id") int id,
                         Model model){
-        // Получим одного человека по его id из DAO и передадим на отображение в представление
-
-        return null;
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
     }
 }
